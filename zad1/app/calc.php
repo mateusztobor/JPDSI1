a9dac531028b4fd5b9ff811467fa55b4131c54e8
Mateusz Tobor (MaTvA) <tobormateusz@gmail.com>
2022-03-07 17:59:05 +0100

    v1
    Kompletne zadanie.

--- zad1/app/calc.php
+++ zad1/app/calc.php
@@ -1,73 +1,29 @@
 <?php
-// KONTROLER strony kalkulatora
-require_once dirname(__FILE__).'/../config.php';
 
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
+$result = '';
 
-// 1. pobranie parametrów
-
-$x = $_REQUEST ['x'];
-$y = $_REQUEST ['y'];
-$operation = $_REQUEST ['op'];
-
-// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
-
-// sprawdzenie, czy parametry zostały przekazane
-if ( ! (isset($x) && isset($y) && isset($operation))) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+//sprawdzanie, czy wszyskie pola formularza istnieją
+if(isset($_POST['post_kwota']) && isset($_POST['post_odsetki']) && isset($_POST['post_mies'])) {
+	//sprawdzanie, czy wymagane pola nie są puste
+	if(!empty($_POST['post_kwota']) && !empty($_POST['post_odsetki']) && !empty($_POST['post_mies'])) {
+		//sprawdzamy czy pola numeryczne mają poprawne wartości
+		if(is_numeric($_POST['post_kwota']) && is_numeric($_POST['post_odsetki']) && is_numeric($_POST['post_mies'])) {
+			//obliczanie raty
+			$rata = ($_POST['post_kwota']/$_POST['post_mies']);
+			$rata += (($rata/100) * $_POST['post_odsetki']);
+			$rata *= 100;
+			$rata = round($rata);
+			$rata /= 100;
+			//wczytywanie rezultatu do zmiennej $result
+			$result .= '<div class="text-center alert alert-secondary mb-4" role="alert">';
+			$result .= '<h3>Wartość miesięcznej raty wynosi <span style="color:red;">'.$rata.'zł</span></h3>';
+			$result .= 'W sumie pożyczając '.$_POST['post_kwota'].'zł na '.$_POST['post_mies'].' miesięcy, przy odsetkach '.$_POST['post_odsetki'].'% spłacisz łącznie '.($rata*$_POST['post_mies']).'zł<br>';
+			$result .= 'Ta pożyczka będzie Cię kosztowała '.(($rata*$_POST['post_mies'])-$_POST['post_kwota']).'zł';
+			$result .= '<br><br><a href="index.php" class="btn btn-light">Wykonaj nowe obliczenie</a>';
+			$result .= '</div>';
+		} else $result .= '<div class="alert alert-danger" role="alert">Podane wartości są niepoprawne!</div>';
+	} else $result .= '<div class="alert alert-danger" role="alert">Nie podano wszystkich wymaganych wartości!</div>';
 }
 
-// sprawdzenie, czy potrzebne wartości zostały przekazane
-if ( $x == "") {
-	$messages [] = 'Nie podano liczby 1';
-}
-if ( $y == "") {
-	$messages [] = 'Nie podano liczby 2';
-}
-
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
-	}
-	
-	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
-	}	
-
-}
-
-// 3. wykonaj zadanie jeśli wszystko w porządku
-
-if (empty ( $messages )) { // gdy brak błędów
-	
-	//konwersja parametrów na int
-	$x = intval($x);
-	$y = intval($y);
-	
-	//wykonanie operacji
-	switch ($operation) {
-		case 'minus' :
-			$result = $x - $y;
-			break;
-		case 'times' :
-			$result = $x * $y;
-			break;
-		case 'div' :
-			$result = $x / $y;
-			break;
-		default :
-			$result = $x + $y;
-			break;
-	}
-}
-
-// 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
-//   będą dostępne w dołączonym skrypcie
+//wywołanie widoku
 include 'calc_view.php';
